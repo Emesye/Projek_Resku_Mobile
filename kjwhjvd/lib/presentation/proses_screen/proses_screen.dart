0b97fc609a38f6a78078bed0c625aa80d99496c3
Emesye <ayupratitiz@gmail.com>
2022-11-19 00:34:28 +0700

    second commit

---
+++ kjwhjvd/lib/presentation/proses_screen/proses_screen.dart
@@ -0,0 +1,349 @@
+import '../proses_screen/widgets/proses_item_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:masayu_s_application6/core/app_export.dart';
+import 'package:masayu_s_application6/widgets/custom_button.dart';
+
+class ProsesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: ColorConstant.whiteA700,
+        body: Container(
+          width: size.width,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    padding: getPadding(
+                      left: 30,
+                      top: 8,
+                      right: 210,
+                      bottom: 8,
+                    ),
+                    decoration: BoxDecoration(
+                      color: ColorConstant.redA700A5,
+                    ),
+                    child: Text(
+                      "Pesanan",
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: ColorConstant.whiteA700,
+                        fontSize: getFontSize(
+                          20,
+                        ),
+                        fontFamily: 'Happy Monkey',
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: getPadding(
+                    left: 25,
+                    top: 6,
+                    right: 25,
+                  ),
+                  child: Text(
+                    "Transaksi",
+                    overflow: TextOverflow.ellipsis,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: ColorConstant.red800,
+                      fontSize: getFontSize(
+                        15,
+                      ),
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    height: getVerticalSize(
+                      1.00,
+                    ),
+                    width: size.width,
+                    margin: getMargin(
+                      top: 8,
+                    ),
+                    decoration: BoxDecoration(
+                      color: ColorConstant.gray800,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: getPadding(
+                      left: 25,
+                      top: 16,
+                      right: 25,
+                    ),
+                    child: Text(
+                      "ID Transaksi",
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: ColorConstant.black900,
+                        fontSize: getFontSize(
+                          15,
+                        ),
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: getPadding(
+                    left: 25,
+                    top: 30,
+                    right: 25,
+                  ),
+                  child: ListView.builder(
+                    physics: BouncingScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: 2,
+                    itemBuilder: (context, index) {
+                      return ProsesItemWidget();
+                    },
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    height: getVerticalSize(
+                      1.00,
+                    ),
+                    width: size.width,
+                    margin: getMargin(
+                      top: 198,
+                    ),
+                    decoration: BoxDecoration(
+                      color: ColorConstant.gray800,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: getPadding(
+                      left: 17,
+                      top: 6,
+                      right: 17,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Padding(
+                          padding: getPadding(
+                            top: 4,
+                            bottom: 5,
+                          ),
+                          child: Text(
+                            "BAYAR",
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: ColorConstant.black900,
+                              fontSize: getFontSize(
+                                15,
+                              ),
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: getPadding(
+                      left: 35,
+                      top: 17,
+                      right: 35,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Text(
+                          "KEMBALI",
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: ColorConstant.black900,
+                            fontSize: getFontSize(
+                              15,
+                            ),
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        Padding(
+                          padding: getPadding(
+                            left: 40,
+                          ),
+                          child: Text(
+                            " Rp. 9.000",
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: ColorConstant.black900,
+                              fontSize: getFontSize(
+                                15,
+                              ),
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    height: getVerticalSize(
+                      44.00,
+                    ),
+                    width: size.width,
+                    margin: getMargin(
+                      top: 12,
+                    ),
+                    child: Stack(
+                      alignment: Alignment.centerRight,
+                      children: [
+                        Align(
+                          alignment: Alignment.center,
+                          child: Container(
+                            margin: getMargin(
+                              right: 1,
+                            ),
+                            decoration: BoxDecoration(
+                              color: ColorConstant.bluegray100,
+                            ),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Padding(
+                                    padding: getPadding(
+                                      left: 99,
+                                      top: 13,
+                                      right: 99,
+                                      bottom: 13,
+                                    ),
+                                    child: Text(
+                                      " Rp. 21.000",
+                                      overflow: TextOverflow.ellipsis,
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                        color: ColorConstant.black900,
+                                        fontSize: getFontSize(
+                                          15,
+                                        ),
+                                        fontFamily: 'Inter',
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        // CustomButton(
+                        //   text: "\"BAYAR\"",
+                        //   margin: getMargin(),
+                        //   alignment: Alignment.centerRight,
+                        // ),
+                        Container(
+                          width: 130,
+                          height: 100,
+                          margin: getMargin(
+                            left: 0,
+                            top: 0,
+                            bottom: 3,
+                          ),
+                          padding: EdgeInsets.only(top: 4),
+                          child: MaterialButton(
+                              child: Text("Bayar"),
+                              minWidth: double.infinity,
+                              height: 45.0,
+                              color: Color.fromARGB(255, 255, 68, 68),
+                              textColor: Colors.white70,
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return AlertDialog(
+                                      title: Text(""),
+                                      content: Text("transaksi berhasil!"),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          child: Text("Okay"),
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              }),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: getPadding(
+                      left: 101,
+                      top: 26,
+                      right: 101,
+                      bottom: 7,
+                    ),
+                    child: Text(
+                      "www.myresku.com",
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: ColorConstant.gray700,
+                        fontSize: getFontSize(
+                          10,
+                        ),
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
