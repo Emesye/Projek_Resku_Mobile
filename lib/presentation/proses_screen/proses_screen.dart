b0b847794dee6a2a8f8fedc85bcb95ae0e337cdc
Emesye <ayupratitiz@gmail.com>
2022-12-15 11:00:13 +0700

    terbaru

--- lib/presentation/proses_screen/proses_screen.dart
+++ lib/presentation/proses_screen/proses_screen.dart
@@ -122,24 +122,23 @@ class ProsesScreen extends StatelessWidget {
                     // )
                     child: ScrollableTableView(
                       columns: [
-                        "id_transaksi",
-                        "nama_barang",
-                        "jumlah",
-                        "harga",
+                        "Barang",
+                        "Jumlah",
+                        "Harga",
                       ].map((column) {
                         return TableViewColumn(
                           label: column,
                         );
                       }).toList(),
                       rows: [
-                        ["PR1000", "jus pisang", "1", "10.000"],
-                        ["PR1000", "nasi goreng", "1", "20.000"],
-                        ["PR1000", "piscok", "1", "8.000"],
-                        ["PR1000", "bakso", "1", "12.000"],
-                        ["PR1000", "karedok", "1", "9.000"],
-                        ["PR1000", "mi goreng", "1", "9.000"],
-                        ["PR1000", "fuyunghai", "1", "19.000"],
-                        ["PR1000", "udang saus mentega", "1", "30.000"],
+                        ["Jus pisang", "1", "10.000"],
+                        ["Nasi goreng", "1", "20.000"],
+                        ["Piscok", "1", "8.000"],
+                        ["Bakso", "1", "12.000"],
+                        ["Karedok", "1", "9.000"],
+                        ["Mie pangsit", "1", "9.000"],
+                        ["Fuyunghai", "1", "19.000"],
+                        ["Udang keju", "1", "30.000"],
                       ].map((record) {
                         return TableViewRow(
                           height: 60,
@@ -151,153 +150,79 @@ class ProsesScreen extends StatelessWidget {
                         );
                       }).toList(),
                     )),
-
-                // body: Center(
-
-                // child: column(children: <Widget>[
-                //   Container(
-                //     margin: EdgeInsets.all(20),
-                //     child: Table(
-                //       defaultColumnWidth: FixedColumnWidth(120.0),
-                //       border: TableBorder.all(
-                //         Color: Colors.black,
-                //         style: BorderStyle.solid,
-                //         width: 2),
-
-                //     children: [
-                //       TableRow( children: [
-                //         Column(children:[Text('Website', style: TextStyle(fontSize:20.0))]),
-                //         Column(children:
-                //         [Text('Tutorial',style: TextStyle(fontSize))]),
-                //         Column(children:[Text('Review', style: TextStyle(fontSize:20.0))]),
-
-                //       ]),
-                //       TableRow( children:[
-                //         Column(children:[text('Javatpoint')]),
-                //         Column(children:[text('Flutter')]),
-                //         Column(children:[Text('5*')]),
-
-                //       ]),
-                //       TableRow( children: [
-                //         Column(children:[Text('Javatpoint')]),
-                //         Column(children:[Text('MySQL')]),
-                //         Column(children:[Text('5*')]),
-
-                //       ])
-                //       TableRow( children: [
-                //         Column(children:[Text('Javapoint')]),
-                //         Column(children:[Text('ReactJS')]),
-                //         Column(children:[Text('5*')]),
-                //       ]),
-                //     ],
-                //     ),
-                //   ),
-                // ]),
-                // );
-
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Container(
-                    height: getVerticalSize(
-                      1.00,
-                    ),
-                    width: size.width,
-                    margin: getMargin(
-                      top: 8,
-                    ),
-                    decoration: BoxDecoration(
-                      color: ColorConstant.gray800,
-                    ),
-                  ),
-                ),
                 // Align(
                 //   alignment: Alignment.centerLeft,
                 //   child: Padding(
-                //     padding: getPadding(
-                //       left: 25,
-                //       top: 16,
-                //       right: 25,
-                //     ),
-                //   )
-                // )
-                //     // child: Text(
-                //     //   "ID Transaksi",
-                //     //   overflow: TextOverflow.ellipsis,
-                //     //   textAlign: TextAlign.left,
-                //     //   style: TextStyle(
-                //     //     color: ColorConstant.black900,
-                //     //     fontSize: getFontSize(
-                //     //       15,
-                //     //     ),
-                //     //     fontFamily: 'Inter',
-                //     //     fontWeight: FontWeight.w400,
-                //     //   ),
-                //     // ),
-                //   ),
-                // ),
-                // Align(
-                //     alignment: Alignment.centerLeft,
-                //     child: SingleChildScrollView(
-                //         // child: ListView(
-                //         //   children: [Text("Test1")],
-                //         // ),
-                //         )),
-                // Padding(
-                //   padding: getPadding(
-                //     left: 25,
-                //     top: 30,
-                //     right: 25,
-                //   ),
-                //   // child: ListView.builder(
-                //   //   physics: BouncingScrollPhysics(),
-                //   //   shrinkWrap: true,
-                //   //   itemCount: 3,
-                //   //   itemBuilder: (context, index) {
-                //   //     return ProsesItemWidget();
-                //   //                         },
-                //   // ),
-                // ),
-                // Align(
-                //   alignment: Alignment.centerLeft,
-                //   child: Container(
-                //     height: getVerticalSize(
-                //       1.00,
-                //     ),
-                //     width: size.width,
-                //     margin: getMargin(
-                //       top: 100,
-                //     ),
-                //     decoration: BoxDecoration(
-                //       color: ColorConstant.gray800,
-                //     ),
-                //   ),
+                //       padding: getPadding(
+                //         left: 0,
+                //         top: 250,
+                //         right: 0,
+                //       ),
+                //       child: Text('BAYAR')
+                //       // Container(
+                //       //   width: 260,
+                //       //   height: 100,
+                //       //   color: Colors.white,
+                //       //   child: Align(
+                //       //       alignment: Alignment.bottomCenter,
+                //       //       child: Text('BAYAR')),
+                //       // ),
+                //       ),
                 // ),
                 Align(
-                  alignment: Alignment.centerRight,
+                  alignment: Alignment.centerLeft,
                   child: Padding(
                     padding: getPadding(
-                      left: 185,
+                      left: 150,
                       top: 0,
                       right: 0,
                     ),
+
+                    // Container(
+                    //       width: 50,
+                    //       height: 50,
+                    //       color: Colors.white,
+                    //       child: Align(
+                    //           alignment: Alignment.centerLeft,
+                    //           child: Text('BAYAR')),
+                    //     ),
                     child: Container(
-                      height: 120,
+                      width: 500,
+                      height: 130,
                       color: Colors.white,
                       alignment: Alignment.bottomLeft,
-                      child: Column(children: [
-                        Text(''),
+                      child: Row(children: [
+                        // Text('BAYAR'),
                         Container(
-                          width: 150,
+                          // child: Text("BAYAR"),
+                          width: 100,
                           height: 100,
                           color: Colors.white,
                           child: Align(
-                            alignment: Alignment.centerRight,
+                            alignment: Alignment.centerLeft,
+                            child: Text('BAYAR'
+                                // textAlign: TextAlign.center,
+                                // decoration: InputDecoration(
+                                //   border: OutlineInputBorder(
+                                //       borderRadius: BorderRadius.circular(25)),
+                                //   hintText: 'Nominal',
+                                // ),
+                                ),
+                          ),
+                        ),
+                        Container(
+                          // child: Text("BAYAR"),
+                          width: 130,
+                          height: 50,
+                          color: Colors.white,
+                          child: Align(
+                            alignment: Alignment.centerLeft,
                             child: TextField(
                               textAlign: TextAlign.center,
                               decoration: InputDecoration(
                                 border: OutlineInputBorder(
                                     borderRadius: BorderRadius.circular(25)),
-                                hintText: 'Jumlah Bayar',
+                                hintText: 'Nominal',
                               ),
                             ),
                           ),
